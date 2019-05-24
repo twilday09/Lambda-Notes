e0789b120b83c5ba1fdae7fe61489d1827baceb1
Joshua Vandergriff <vanditechnician@gmail.com>
2019-05-23 20:15:37 -0500

    Completed data_types tutorial.

---
+++ CodingPhase/Future-Proof-JavaScript/Data_Types.js
@@ -0,0 +1,40 @@
+//Data Types
+
+// Strings
+/* Strings are any type of data (numbers, letters, symbols) that are all compromised inside of a set of either single or double quotes. However, it is
+good practice when choosing quote methodology to keep it consistent in your project/documents.
+/*Example*/ var name = 'Joe';
+
+// Numbers/Integers
+/* Numbers are written without any sort of bracketing/holding characteristic as noted below.
+/*Example*/ var number = 23
+
+//Arrays
+var array = ['Joe' 23, ['hey', 'Joe']]
+/* Arrays are essentially 'lists' of data. An array can hold any time of data such as: Strings, numbers, and even other arrays. Arrays are noticed
+by their use of the square brackets.*/
+
+//Objects
+var object = {
+    key: 'value',
+    func: function() {
+        return 'hey'
+    }
+}
+/* An object is a data type that is responsible for holding what is known as 'Keys' and 'Values. The 'Key' can essentially be the name and the 'Value'
+is what you wish to assign to it. An object may hold things such as strings, numbers, booleans, and even functions.*/
+
+//Booleans
+var Booleans = true;
+/* A boolean is essentially a true/false statement. Often used when comparing data.*/
+
+//Null
+var nullable = null;
+/* Null means that you have created a piece of code that holds a value inside, however at this given time that value does not have a set corespondance.
+This is good for using as placeholder.*/
+
+//Undefined
+var another = undefined;
+/* Undefined literally means just that. It is often used to search your code and find wether or not a var is used. 
+
+
