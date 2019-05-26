2080f9af7baaecede57f942095b851293d4b1a3b
Joshua Vandergriff <vanditechnician@gmail.com>
2019-05-26 13:02:54 -0500

    Completed the arrays excercises tutorial.

---
+++ CodingPhase/Future-Proof-JavaScript/Arrays/Array_Excercise_Answers.js
@@ -0,0 +1,12 @@
+//Declare a variable with 5 different music artists
+let artists = ['Eminem', 'Snoop Dog', 'Beyonce', 'Jay-Z', 'Hopsin']
+//Print the variable
+console.log(artists);
+//Print the 4th artist on the array with a console.log
+console.log(artists[3])
+//Add another artist to array for a total of 6 artists then print the array
+artists.push('Any Name');
+//Remove the 4th artist on the list and print the list
+artists.splice(3,1)
+//Print the array in reverse
+console.log(artists.reverse());
