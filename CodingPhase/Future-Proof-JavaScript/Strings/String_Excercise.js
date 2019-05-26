ad85dceff3713f1853a085f704ace9a31fdaa056
Joshua Vandergriff <vanditechnician@gmail.com>
2019-05-26 11:35:58 -0500

    Completed the String_excercise tutorial.

---
+++ CodingPhase/Future-Proof-JavaScript/Strings/String_Excercise.js
@@ -0,0 +1,10 @@
+// Declare a variable with your first name
+let firstName = 'Joshua'
+// Console.log(yourname) with all capital letters
+console.log(firstName.toUpperCase());
+// Console.log(yourname) with all lower case letters
+console.log(firstName.toLocaleLowerCase());
+// Declare a variable with your last name
+let lastName = 'Vandergriff'
+// Concat (merge) both your first and last name
+console.log(firstName + " " + lastName);
